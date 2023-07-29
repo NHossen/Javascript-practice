d94231f8292920077d1d2485ef056f14d5e5d544
NHossen <naeemhossen2509@gmail.com>
2023-07-29 22:11:46 +0400

    multipale-conditions

--- conditional.js
+++ conditional.js
@@ -3,7 +3,21 @@ var myBudget=9500;
 
 // if iphone price is less then my budged i will buy the iphone
 
+ 
+
+var chikenPrice=500;
+var money=50;
+// if (chikenPrice<=money){
+//     console.log("murgikhabo");
+// }
+
+// if (chikenPrice>money){
+//99*///     console.log("Jankar");
+// }
+
+
+
 /**
  * 
  * 
- * */ 
+ * */
---
+++ multiple-conditons.js
@@ -0,0 +1,12 @@
+var isGraduated=true;
+var salary=35000;
+
+if(isGraduated ==true && salary>50000){
+    console.log('aso biye korii')
+}
+
+else{
+    console.log(
+        'tor kopale biye anii'
+    )
+}
