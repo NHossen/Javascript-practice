9d6176545778288a40046e9f3be8dc71302c528a
NHossen <naeemhossen2509@gmail.com>
2023-07-29 21:23:22 +0400

    array pop

--- push-pop.js
+++ push-pop.js
@@ -1,8 +1,14 @@
 var numbers=[78,45,98,45];
 
-// array push
+// array push yo add to an array in last 
 numbers.push(63);
 console.log(numbers);
 var friends=["nur" ,'sana' , 'riad'];
 friends.push("rahim");
-console.log(friends)
+// console.log(friends)
+
+//use  array pop to get last element
+
+var friends=["nur" ,'sana' , 'riad'];
+friends.pop();
+console.log(friends);
