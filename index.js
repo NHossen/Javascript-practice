b142c0f805ab3a50e93408f4c3da6041b10f7ebc
NHossen <naeemhossen2509@gmail.com>
2023-07-29 21:07:06 +0400

    Get element by index

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 // index is like menu by index you can accses in array
-var number=[45,68,78,56,89,98];
+var numbers=[45,68,78,56,89,98];
+// get element by index
+// var second =numbers[1];
+// console.log(second);
+// numbers[1]=88;
+
+
+// 1:Get element by index
+var element=numbers[1];
+console.log(element);
+// 2.set element value by index
+numbers[1]=77;
+numbers[5]=57885;
+console.log(numbers);
