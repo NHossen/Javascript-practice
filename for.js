8541fb028d7724bc0eb9ea521d5fc6012cc440d9
NHossen <naeemhossen2509@gmail.com>
2023-07-31 00:07:46 +0400

    by for loop check odd and even number

---
+++ for.js
@@ -0,0 +1,18 @@
+// var roastGiven=0;
+// while(roastGiven<7){
+//     console.log("roast given");
+// }
+
+// for (var roastGiven=0;roastGiven<7 ; roastGiven++){
+//     console.log("roast den");
+// }
+
+// simple version of for loop
+// console.log('simple for loop')
+// for(var i=0;i<7;i++){
+//     console.log('sdkjksjdgsdgdgs');
+// }
+
+for(var i=0; i<=10; i=i+2){
+    console.log(i);
+}
