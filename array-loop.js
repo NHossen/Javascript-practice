2a7c4d8f494641f95fca523ff5a1aa4ef99ae0ca
NHossen <naeemhossen2509@gmail.com>
2023-07-31 00:19:49 +0400

    by for loop check array

---
+++ array-loop.js
@@ -0,0 +1,12 @@
+// for(var i=0 ; i<10; i++){
+//     console.log(i);
+// }
+
+// display
+var numbers=[45,78,89,56,32,25,676,676,464,4353,3535]
+
+for(var i=0; i<numbers.length;i++){
+    var number=numbers[i];
+    console.log(number);
+
+}
