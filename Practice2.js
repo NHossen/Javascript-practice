5b6ff10482c0f6ee585def387548b7c523da23d3
NHossen <naeemhossen2509@gmail.com>
2023-07-30 21:28:48 +0400

    practic 2 Completed

---
+++ Practice2.js
@@ -0,0 +1,61 @@
+var naeem=85;
+var tom=55;
+var jane=95;
+var peter=56;
+var john=40;
+
+// var  personName=[naeem ,tom,jane,peter,john]
+
+if (naeem > 80 ){
+    console.log('I got A Grade ');
+}
+else if (naeem > 60 ){
+    console.log('I got B Grade ');
+}
+else if (naeem > 50 ){
+    console.log('I got C Grade ');
+}
+else if (naeem > 45 ){
+    console.log('I got D Grade ');
+}
+else {
+    console.log('I got Fail ');
+}
+
+// Tom
+if (tom > 80 ){
+    console.log('I got A Grade ');
+}
+else if (tom > 60 ){
+    console.log('I got B Grade ');
+}
+else if (tom > 50 ){
+    console.log('I got C Grade ');
+}
+else if (top > 45 ){
+    console.log('I got D Grade ');
+}
+else {
+    console.log('I got Fail ');
+}
+
+// Jane
+if (jane > 80 ){
+    console.log('I got A Grade ');
+}
+else if (jane > 60 ){
+    console.log('I got B Grade ');
+}
+else if (jane> 50 ){
+    console.log('I got C Grade ');
+}
+else if (jane > 45 ){
+    console.log('I got D Grade ');
+}
+else {
+    console.log('I got Fail ');
+}
+
+
+
+// console.log(personName);
