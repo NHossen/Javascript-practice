9fc60cd633ac4b13e3be933b10998706628807d4
NHossen <naeemhossen2509@gmail.com>
2023-07-30 21:46:18 +0400

    revision completed

---
+++ Javascript-loops/revision.js
@@ -0,0 +1,36 @@
+
+// String type variable
+var bottle='water';
+var bottleColor='yellow';
+
+// Number type variable
+var sunglassPrice=120;
+var penPrice=20;
+
+// Boolean type
+var isExpensive=true;
+
+// Array
+
+var tableItems=['bottle','pen','sunglasses','pappers'];
+// Array Starting from Number 0
+
+// Get items from array by index
+
+var item3=tableItems[3];
+tableItems[4]='notebook'
+var penindex=tableItems.indexOf('pen');
+
+
+// Conditionals
+
+if(tableItems.length>4){
+
+}
+else if (tableItems[3]=='pen'){
+
+}
+else{
+    
+}
+
