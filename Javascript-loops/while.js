66ff6e5d0e00289bc19252433a7525f5bf424ed8
NHossen <naeemhossen2509@gmail.com>
2023-07-30 22:11:55 +0400

    while Loops Started

---
+++ Javascript-loops/while.js
@@ -0,0 +1,13 @@
+// While Loops
+
+var roastGiven=0;
+// if(roastGiven<7){
+
+// }
+while(roastGiven<7){
+    console.log('roast den,please')
+    // roastGiven=roastGiven+1;
+    // roastGiven+=1;
+    console.log(roastGiven);
+    roastGiven++;
+}
