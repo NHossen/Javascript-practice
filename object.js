296325711ccf1b657470eb7dbec1f9c6feb6c800
NHossen <naeemhossen2509@gmail.com>
2023-08-02 00:20:40 +0400

    javascript object

---
+++ object.js
@@ -0,0 +1,15 @@
+var student={
+
+    id:115,
+    name:'Naeem Hossen',
+    FavColor:'red'
+    ,}
+
+var mobile={
+    brand:'Iphone',
+    price:1500,
+    camera:'7MP'
+}
+
+// Objective property by consle
+console.log(mobile.price);
--- odd.js
+++ odd.js
@@ -1,6 +1,7 @@
-var number=1;
-while (number<=30){
-    console.log(number);
-    // number++;
-    number=number+2;
-}
+// var number=1;
+// while (number<=30){
+//     console.log(number);
+//     // number++;
+//     number=number+2;
+// }
+
