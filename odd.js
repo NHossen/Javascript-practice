e6cfa5c92fea2067457af4b6f065d1d048509eab
NHossen <naeemhossen2509@gmail.com>
2023-08-03 01:02:57 +0400

    odd or even number Chek

--- odd.js
+++ odd.js
@@ -5,3 +5,25 @@
 //     number=number+2;
 // }
 
+
+// Odd and even number check by Function
+
+// Even number check
+
+function isEven(number) {
+
+    const reminder=number%2;
+
+    // console.log(reminder);
+    if (reminder===0) {
+        // console.log('This is Even Number');
+        return true;
+        
+    }
+    else{
+        // console.log('this is odd');
+        return false
+    }
+    
+}
+isEven(303);
