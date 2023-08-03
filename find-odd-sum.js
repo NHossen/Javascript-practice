ed41c676c238d716b7de4f018bd55bb433e8f6c5
NHossen <naeemhossen2509@gmail.com>
2023-08-03 11:28:17 +0400

    odd numbers sum

--- find-odd-sum.js
+++ find-odd-sum.js
@@ -8,7 +8,7 @@ function getSum(numbers) {
         const arrayAndNumbers=numbers[indexNumbers];
         // sum+=arrayAndNumbers;
         sum=sum+arrayAndNumbers;
-        console.log(indexNumbers,arrayAndNumbers,sum);
+        // console.log(indexNumbers,arrayAndNumbers,sum);
         
     }
     return sum
@@ -18,3 +18,33 @@ function getSum(numbers) {
 // Numbers Declare by Variable
 const myArrayNum=[12,65,45,78,32,45,91];
 getSum(myArrayNum);
+
+
+// get odd numbers sum from a array...
+
+function getOddNumbersOfArray(arr) {
+
+    // odd number array
+    const oddNumbers=[];
+
+    for (let i = 0; i < arr.length; i++) {
+
+        const indexArraynum = i;
+        const arrayNum=arr[indexArraynum];
+        if (arrayNum % 2 !== 0) {
+
+            console.log(indexArraynum,arrayNum);
+            oddNumbers.push(arrayNum);
+            
+        }
+        // console.log(indexArraynum,arrayNum);
+        
+    }
+    return oddNumbers;
+    
+}
+const arrayNumbers=[12,65,45,78,32,45,91];
+const oddNumbers=getOddNumbersOfArray(arrayNumbers);
+console.log(oddNumbers);
+const oddNumbersSum=getSum(oddNumbers);
+console.log(oddNumbersSum);
