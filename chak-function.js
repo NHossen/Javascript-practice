518248c7504d271b9751e5c27476815023e2295b
NHossen <naeemhossen2509@gmail.com>
2023-08-04 09:47:11 +0400

    function chack by object values

---
+++ chak-function.js
@@ -0,0 +1,14 @@
+function myFunc(theObject) {
+    theObject.make = "Toyota";
+  }
+  
+  const mycar = {
+    make: "Honda",
+    model: "Accord",
+    year: 1998,
+  };
+  
+  console.log(mycar.make); // "Honda"
+  myFunc(mycar);
+  console.log(mycar.make); // "Toyota"
+  
