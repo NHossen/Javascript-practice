84999e7b86e773c63a9b77de5d6684d8ebc9f102
NHossen <naeemhossen2509@gmail.com>
2023-08-06 16:16:58 +0400

    99 bottels of bear song solution by conditions

--- conditional.js
+++ conditional.js
@@ -223,4 +223,29 @@ switch(color){
     //Do NOT change any of the code below 👇
     }
     console.log(fibonacciGenerator (10));
+
+
+                    // 99 bottels of bear code sollutions
+
+
+    var bottles;
+for (let counter = 1; counter >= 1; counter = counter - 1) 
+{
+    if (counter == 1) {
+        bottles = 'bottle';
+    } else {
+        bottles = 'bottles';
+    }
+    console.log(counter+" "+bottles+" of beer on the wall.");
+    if (counter < 99) {
+        console.log("");
+        console.log(counter+" "+bottles+" of beer on the wall.");
+    }
+    console.log(counter+" "+bottles+" of beer.");
+    console.log("Take one down.");
+    console.log("Pass it around.");
+    if (counter == 1) {
+        console.log("No bottles of beer on the wall.");
+    }
+}
                          
