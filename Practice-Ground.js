7a2ca7bb893cf883146ec4cd5112916fd328fe80
Naeem Hossen <132334814+NHossen@users.noreply.github.com>
2023-08-26 16:40:35 +0400

    learning about api

--- Practice-Ground.js
+++ Practice-Ground.js
@@ -1,4 +1,22 @@
-for(let i=5;i<=100;i++);{
+const nu =[1,2,3,43,3,4242,23];
+const x=nu.find((item)=>item===43)
+console.log(x);
+
+//filter
+const numbers=[1,2,3,45,4,345,34,364,23];
+
+const filter=numbers=>{
+    const result=[];
+
+    for(let item of numbers){
+        if(item%2===0){
+            result.push(item);
+        }
+    }
+    return result;
 }
+ const evenx=filter(numbers);
+ console.log(evenx);
+
 
 
